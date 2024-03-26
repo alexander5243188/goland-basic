9fc625a01d1b39ee7e344e147000475fbc8f2d36
Alexander Richard Vega Fuentes <alexander5243188@yahoo.es>
2024-03-26 11:39:32 -0400

    keywords continue, break

--- keywords/keywords.go
+++ keywords/keywords.go
@@ -9,4 +9,18 @@ func main() {
 	fmt.Println("programando")
 	defer fmt.Println("en")
 	fmt.Println("GO")
+
+	//continue, break
+	count := 10
+	for i := 0; i < count; i++ {
+		fmt.Println(i)
+		if i == 3 {
+			fmt.Println("-", i, "-")
+			continue
+		}
+		if i == 5 {
+			fmt.Println("No terminaras el ciclo JAJAJAJa")
+			break
+		}
+	}
 }
