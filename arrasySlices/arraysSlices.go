12dbba614b23531e18e74ea913e99198f478fe22
Alexander Richard Vega Fuentes <alexander5243188@yahoo.es>
2024-03-26 16:53:54 -0400

    Slice, estructura de datos dinamica

--- arrasySlices/arraysSlices.go
+++ arrasySlices/arraysSlices.go
@@ -25,4 +25,12 @@ func main() {
 	fmt.Println("Tamaño del arreglo ", len(arreglo), " elementos")
 	fmt.Println("Capacidad del arreglo", cap(arreglo), " elementos")
 
+	estructura_datos_dinamica := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	fmt.Println("Estructura de datos dinamica", estructura_datos_dinamica)
+	fmt.Println("Tamaño de la estructura dinamica ", len(estructura_datos_dinamica))
+	fmt.Println("Capacidad dela estructura dinamica ", cap(estructura_datos_dinamica))
+	fmt.Println("[0]", estructura_datos_dinamica[0])
+	fmt.Println("[:3]", estructura_datos_dinamica[:3])
+	fmt.Println("[2:7]", estructura_datos_dinamica[2:7])
+	fmt.Println("[5:8]", estructura_datos_dinamica[5:8])
 }
